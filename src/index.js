bf4fa7850d9250b06c9e1764e67eba145e8d37f1
tushartiwari7 <tushartiwari0172@gmail.com>
2022-05-11 16:02:29 +0530

    removed mock-backend

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { makeServer } from "./server";
-
-// Call make Server
-makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
