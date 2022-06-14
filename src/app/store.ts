517503d56def8558fb76767cf1daf7f47013417e
tushartiwari7 <tushartiwari0172@gmail.com>
2022-06-14 11:38:16 +0530

    [chore] created types for  Redux Store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import auth from './features/authSlice';
 import users from './features/userSlice';
 import tweets from './features/tweetSlice';
 import bookmarks from './features/bookmarkSlice';
-export const store:any = configureStore({
+
+export const store = configureStore({
 	reducer: {
 		auth,
 		users,
@@ -11,3 +13,12 @@ export const store:any = configureStore({
 		bookmarks
 	}
 });
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {auth, users, tweets, bookmarks}
+export type AppDispatch = typeof store.dispatch;
+
+// Use throughout the app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
